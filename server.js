4b443e3ad1ddbead1182d0aa548e1b0f4bc5d6ed
Satyam Pandey <satyam53@tsecol.onmicrosoft.com>
2026-02-22 10:30:42 +0530

    Fix #730: Implement Distributed Transaction Integrity & Vector-Clock Reconciler

--- server.js
+++ server.js
@@ -79,6 +79,8 @@ async function connectDatabase() {
         require('./jobs/conflictCleaner').start();
         require('./jobs/logRotator').start();
         require('./jobs/searchIndexer').start();
+        require('./jobs/conflictPruner').start();
+
 
 
         // Start resilient orchestrator
@@ -112,7 +114,8 @@ app.use('/api/export', require('./routes/export'));
 app.use('/api/forecasting', require('./routes/forecasting'));
 app.use('/api/governance', require('./routes/governance'));
 app.use('/api/taxonomy', require('./routes/taxonomy'));
-app.use('/api/sync', require('./routes/syncManager'));
+app.use('/api/sync', require('./routes/sync'));
+
 app.use('/api/telemetry', require('./routes/telemetry'));
 app.use('/api/jobs', require('./routes/jobs'));
 
